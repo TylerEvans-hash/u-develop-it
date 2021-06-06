53b2ed5d6c2df1cb241599ec1050bab244dd6350
Red3ffect <red3ffect117@gmail.com>
2021-06-05 22:21:16 -0700

    Adds delete and insert queries

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express');
 const mysql = require('mysql2');
 // const { prototype } = require('mysql2/typings/mysql/lib/protocol/sequences/Sequence');
@@ -21,9 +22,35 @@ const db = mysql.createConnection(
 );
 
 // Routes here
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidates
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
